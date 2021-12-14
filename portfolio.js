7f2f3bbc9187b18718a700db5d97eff5a12eca8d
saru <saru.gmesp@gmail.com>
2021-12-14 23:01:49 +0100

    Protfolio created

---
+++ portfolio.js
@@ -0,0 +1,173 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Sarulatha Anbu",
+	title: "Hi all, I'm Sarulatha ",
+	description:
+		"A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs.",
+	resumeLink: "https://github.com/Sarulathaanbu/cv/blob/master/Sarulatha%20FE%20Resume.pdf",
+};
+
+export const openSource = {
+	githubUserName: "SarulathaAnbu",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+
+	
+	github: "https://github.com/SarulathaAnbu",
+	linkedin: "https://www.linkedin.com/in/sarulatha-anbu/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end and Back end for your web page"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Anna University",
+		subHeader: "Bachelor of Engineering in Information Technology",
+		duration: "August 2002 - June 2006",
+	
+		
+	},
+];
+
+export const experience = [
+	{
+		role: "Salesforce Administrator",
+		company: "HumanAid",
+		companylogo: "/img/icons/common/google.svg",
+		date: "November 2021 – Present",
+		
+		descBullets: [
+			"Developed reports, dashboards, and processes to continuously monitor data quality and integrity",
+"Analyzed current processes and made recommendations for automated notifications to Sales Reps and when outstanding invoices need attention, standardised sales reps reports",
+			
+		],
+	},
+	{
+		role: "Full Stack Developer",
+		company: "Hack Your Future",
+		companylogo: "/img/icons/common/github.svg",
+		date: "March 2021 – Present",
+		descBullets: [ 
+			"Intensive learning with fully practical skills on full stack web development",
+		"Participate on different events, workshops, webinars & study-groups with professional & senior Software Engineers",
+		"Learning modern Front end and back end technologies with weekly assignments & projects",
+	],
+	},
+	{
+		role: "Frontend Devoloper",
+		company: "Comet Softwares",
+		companylogo: "/img/icons/common/airbnbLogo.png",
+		date: "June 2009 – June 2010",
+		descBullets: [
+			"Worked tightly with project management and front-end development on MVC projects such as the company website and a kiosk solution",
+"Provided UX consultancy for in-house developed products on mobile friendly and graphic assistance for company’s social media branding",
+			
+		],
+	},
+];
+
+export const projects = [
+	{
+		name: "Meal Sharing App",
+		desc: "Meal-sharing app is basically like Airbnb for meals.",
+		link: "https://meal-sharing-saru.herokuapp.com/",
+	},
+	{
+		name: "Weather App",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/Sarulathaanbu",
+	},
+	{
+		name: "Gif Search",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/Sarulathaanbu",
+		link: "https://example.com",
+	},
+];
+
+
